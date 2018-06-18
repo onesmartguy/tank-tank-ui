9ebb689a9d04f4ccda41a8a9767577c75e459085
Eddie Flores <eddie@iloves.io>
2018-06-18 19:23:39 +0000

    init

---
+++ nuxt.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  srcDir: "src/",
+  head: {
+    title: "Tank Tank",
+    titleTemplate: "%s - Nuxt.js",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "Meta description" },
+
+      { rel: "stylesheet", href: "/css/index.css" },
+      { rel: "favicon", href: "favicon.ico" }
+    ]
+  }
+};
